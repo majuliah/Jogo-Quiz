fe5af3b3ccdd0ce4861fc6e882345e5e1d0c399a
majulha <majulialcarvalho@gmail.com>
2023-08-29 11:07:47 -0300

    iniciando a implementação da interface e a regra de perguntas e respostas

--- 001 - Inicio/Quiz Project/Assets/SeletorDeEnigmas.cs
+++ 001 - Inicio/Quiz Project/Assets/SeletorDeEnigmas.cs
@@ -1,19 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SeletorDeEnigmas : MonoBehaviour
 {
     [SerializeField] ListaDeEnigmas lista;
+    [SerializeField] Text perguntaTexto;
+    [SerializeField] Text botaoResposta1Texto;
+    [SerializeField] Text botaoResposta2Texto;
+    [SerializeField] Text botaoResposta3Texto;
+    [SerializeField] Text botaoResposta4Texto;
+    [SerializeField] Text botaoResposta5Texto;
+    public List<string> respostasPossiveis = new List<string>();
+    public int index;
+    public int indexRespostas;
+
     void Start()
     {
-        int index = Random.Range(0, lista.listaDeEnigmas.Count);
-        Debug.Log(lista.listaDeEnigmas[index].pergunta);
-        Debug.Log(lista.listaDeEnigmas[index].respostaCorreta);
-        Debug.Log(lista.listaDeEnigmas[index].respostaErrada1);
-        Debug.Log(lista.listaDeEnigmas[index].respostaErrada2);
-        Debug.Log(lista.listaDeEnigmas[index].respostaErrada3);
-        Debug.Log(lista.listaDeEnigmas[index].respostaErrada4);
+        index = Random.Range(0, lista.listaDeEnigmas.Count);
+        
+        //populando a lista respostasPossiveis com as nossas respostas
+        respostasPossiveis.Add(lista.listaDeEnigmas[index].respostaCorreta);
+        respostasPossiveis.Add(lista.listaDeEnigmas[index].respostaErrada1);
+        respostasPossiveis.Add(lista.listaDeEnigmas[index].respostaErrada2);
+        respostasPossiveis.Add(lista.listaDeEnigmas[index].respostaErrada3);
+        respostasPossiveis.Add(lista.listaDeEnigmas[index].respostaErrada4);
+        
+        //criando um novo índice para pegar um elemento da lista anterior e setar a um botão 
+        indexRespostas = Random.Range(0, respostasPossiveis.Count);
+        
+        perguntaTexto.text = lista.listaDeEnigmas[index].pergunta;
+        
+        //após atribuir um valor ao botão, vamos remover ele da lista para que o valor não se repita nos botões
+        botaoResposta1Texto.text = respostasPossiveis[indexRespostas];
+        Remove();
+        Randomizar();
+        
+        botaoResposta2Texto.text = respostasPossiveis[indexRespostas];
+        Remove();
+        Randomizar();
+        
+        botaoResposta3Texto.text = respostasPossiveis[indexRespostas];
+        Remove();
+        Randomizar();
+        
+        botaoResposta4Texto.text = respostasPossiveis[indexRespostas];
+        Remove();
+        Randomizar();
+        
+        botaoResposta5Texto.text = respostasPossiveis[indexRespostas];
+        Remove();
+        
+    }
+    public void Remove() => respostasPossiveis.Remove(respostasPossiveis[indexRespostas]);
+    public void Randomizar() => indexRespostas = Random.Range(0, respostasPossiveis.Count);
+    
+    public void OnClick(Text TextoBotao)
+    {
+        if (TextoBotao.text == lista.listaDeEnigmas[index].respostaCorreta)
+        {
+            lista.listaDeEnigmas.Remove(lista.listaDeEnigmas[index]);
+            Start();
+        }
+        else
+            Debug.Log("RESPOSTA ERRADA");
     }
     
 }
